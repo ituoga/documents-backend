488db0eb5d658392640b62d56b5c0992d819559a
manaxi <mantas.peciulis17@gmail.com>
2023-01-14 00:14:44 +0200

    m

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -40,6 +40,7 @@
     <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
         <div class="container">
             <div class="row justify-content-center">
+
                 {{ $slot }}
             </div>
         </div>
