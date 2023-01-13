3f67d123e2900fb42e6809c1fdf150c161dd9ef9
manaxi <mantas.peciulis17@gmail.com>
2023-01-14 00:33:05 +0200

    request storelogin

---
+++ app/Http/Requests/User/StoreLoginRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreLoginRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+        ];
+    }
+}
---
+++ app/Http/Requests/User/StoreRegisterRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRegisterRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+}
